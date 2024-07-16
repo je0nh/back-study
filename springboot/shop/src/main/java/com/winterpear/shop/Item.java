829af35b02fd5e80ee69db3405818193e2bc428b
je0nh <winter0pear@gmail.com>
2024-07-17 00:25:39 +0900

    form으로 받은 데이터 DB로 넣기

--- springboot/shop/src/main/java/com/winterpear/shop/Item.java
+++ springboot/shop/src/main/java/com/winterpear/shop/Item.java
@@ -1,15 +1,19 @@
 package com.winterpear.shop;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @ToString
+@Getter
+@Setter
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer id;
+    private Integer id;
     
     @Column(columnDefinition = "TEXT")
-    public String title;
-    public Integer price;
+    private String title;
+    private Integer price;
 }
--- springboot/shop/src/main/java/com/winterpear/shop/ItemController.java
+++ springboot/shop/src/main/java/com/winterpear/shop/ItemController.java
@@ -5,8 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,7 +23,7 @@ public class ItemController {
     String list(Model model) {
 //        JPS로 데이터베이스에서 데이터 출력하기
         List<Item> result = itemRepository.findAll();
-        System.out.println(result.get(0).price);
+//        System.out.println(result.get(0).price);
         var a = new Item();
         System.out.println(a.toString());
 
@@ -27,5 +32,29 @@ public class ItemController {
         
         model.addAttribute("items", result);
         return "list.html";
+    }
+    
+    @GetMapping("/write")
+    String write(Model model) {
+        return "write.html";
+    }
+
+    @PostMapping("/product")
+    String productPost(@RequestParam Map<String, String> formData) {
+        
+//        map 자료형 이용하기
+        Map<String, Object> test = new HashMap<>();
+        
+        test.put("name", "kim");
+        test.put("age", 20);
+        System.out.println(test.get("name"));
+        
+//        DB 저장
+        Item item = new Item();
+        item.setTitle(formData.get("title"));
+        item.setPrice(Integer.parseInt(formData.get("price")));
+        itemRepository.save(item);
+        
+        return "redirect:/list";
     }
 }
