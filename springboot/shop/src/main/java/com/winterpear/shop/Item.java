6f73d76146d3061c3514f06a1a93ec76478acd55
je0nh <winter0pear@gmail.com>
2024-07-19 03:37:52 +0900

    gettet, setter 추가

--- springboot/shop/src/main/java/com/winterpear/shop/Item.java
+++ springboot/shop/src/main/java/com/winterpear/shop/Item.java
@@ -16,4 +16,28 @@ public class Item {
     @Column(columnDefinition = "TEXT")
     private String title;
     private Integer price;
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
 }
