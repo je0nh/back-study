b18f3c76895b1d8285a496db5920c82b65572720
je0nh <winter0pear@gmail.com>
2024-07-19 03:39:43 +0900

    ItemService를 이용한 DI 구현

---
+++ springboot/shop/src/main/java/com/winterpear/shop/ItemService.java
@@ -0,0 +1,46 @@
+package com.winterpear.shop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+    
+    private final ItemRepository itemRepository;
+    private final EntityManagerFactory emf;
+    
+    public void saveItem (String title, Integer price) {
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);
+    }
+    
+    public List<Item> product () {
+        return itemRepository.findAll();
+    }
+    
+    public Optional<Item> productId (Integer id) {
+        return itemRepository.findById(id);
+    }
+    
+//    DB 데이터 변경
+    public void modifyItem (Integer id, String newTitle, Integer newPrice) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        
+        Item item = em.find(Item.class, id);
+        
+        item.setTitle(newTitle);
+        item.setPrice(newPrice);
+        em.getTransaction().commit();
+        
+        em.close();
+    }
+}
