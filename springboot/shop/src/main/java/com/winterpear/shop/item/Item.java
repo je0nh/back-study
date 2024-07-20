486792154a55d2d306c23c66504dd5b25bd19bd9
je0nh <winter0pear@gmail.com>
2024-07-20 17:14:54 +0900

    getter, setter 삭제

---
+++ springboot/shop/src/main/java/com/winterpear/shop/item/Item.java
@@ -0,0 +1,19 @@
+package com.winterpear.shop.item;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@Setter
+public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(columnDefinition = "TEXT")
+    private String title;
+    private Integer price;
+}
