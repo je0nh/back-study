fe25229328f6ff026946a1dcb98b0da0efb05fc5
je0nh <winter0pear@gmail.com>
2024-07-31 18:44:17 +0900

    회원 기능 - 등록, 조회

---
+++ springboot/hello-spring/src/main/java/hello/hello_spring/controllor/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.controllor;
+
+public class MemberForm {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
