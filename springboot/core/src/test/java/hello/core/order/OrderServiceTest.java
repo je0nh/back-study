306f8d424e9a6d17d6f2f2fd0c14030a9223c188
je0nh <winter0pear@gmail.com>
2024-08-02 17:42:44 +0900

    DI 구현, Spring Container 사용

--- springboot/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ springboot/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,28 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
     
-    MemberService memberService = new MemberServiceImpl();    
-    OrderService orderService = new OrderServiceImpl();
+//    MemberService memberService = new MemberServiceImpl();    
+//    OrderService orderService = new OrderServiceImpl();
+    
+    MemberService memberService;
+    OrderService orderService;
+    
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
     
     @Test
     void createOrder() {
@@ -18,9 +30,9 @@ public class OrderServiceTest {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
         
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 5000);
         
-        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(500);
         Assertions.assertThat(order.getMemberId()).isEqualTo(memberId);
         Assertions.assertThat(order.getItemName()).isEqualTo("itemA");
     }
