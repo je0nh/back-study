1ca8231a8f50178b0757378445c3a12d9ed445ba
je0nh <winter0pear@gmail.com>
2024-08-08 23:54:13 +0900

    롬복을 이용한 의존관계 자동주입

--- springboot/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ springboot/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,25 +1,30 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor // Lombok이 생성자를 자동으로 만들어줌 아주 good.. 영환 센세의 칭찬
 public class OrderServiceImpl implements OrderService {
     
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    
+    @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -32,7 +37,7 @@ public class OrderServiceImpl implements OrderService {
     }
     
     // 테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
+//    public MemberRepository getMemberRepository() {
+//        return memberRepository;
+//    }
 }
